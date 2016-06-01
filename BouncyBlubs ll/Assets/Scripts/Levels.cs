fa7f8ca5fbf54e38c718eef4ae55b5402064c622
MarijnMetzlar <marijn_jacob@hotmail.com>
2016-06-01 13:33:10 +0200

    Added ItemScreen, plus some art for that
    Need UI stuff, will now work on Katana stuff

---
+++ BouncyBlubs ll/Assets/Scripts/Levels.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour {
+
+	public int LevelType;
+	public bool completedLevel = false;
+
+	void OnMouseDown()
+	{
+		if (LevelType == 1) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:3);
+		}
+
+		if (LevelType == 2 && Controls.completedLevel1 == true) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:4);
+		}
+
+		if (LevelType == 3 && Controls.completedLevel2 == true) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:5);
+		}
+
+		if (LevelType == 4 && Controls.completedLevel3 == true) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:6);
+		}
+
+		if (LevelType == 5 && Controls.completedLevel4 == true) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:7);
+		}
+
+		if (LevelType == 6 && Controls.completedLevel5 == true) 
+		{
+			SceneManager.LoadScene (sceneBuildIndex:8);
+		}
+	}
+}
